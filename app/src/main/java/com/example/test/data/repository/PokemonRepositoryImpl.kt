900c5e166c9c1aa197cfbc85d7e6fbfde81aa9ac
LuckyHariharan <lharihar@uwaterloo.ca>
2024-02-18 17:04:42 +0530

    add logs

--- app/src/main/java/com/example/test/data/repository/PokemonRepositoryImpl.kt
+++ app/src/main/java/com/example/test/data/repository/PokemonRepositoryImpl.kt
@@ -7,8 +7,6 @@ import com.example.test.data.remote.PokemonApiService
 import com.example.test.domain.model.Pokemon
 import retrofit2.Response
 
-// data/repository/PokemonRepositoryImpl.kt
-// data/repository/PokemonRepositoryImpl.kt
 class PokemonRepositoryImpl(
     private val apiService: PokemonApiService,
     private val pokemonDao: PokemonDao
@@ -16,7 +14,13 @@ class PokemonRepositoryImpl(
 
     override suspend fun getPokemonFromAPI(): Response<List<Pokemon>> {
         Log.d("Repository", "Fetching Pokemon from API")
-        return apiService.getPokemon()
+        val response = apiService.getPokemon()
+        if (response.isSuccessful) {
+            Log.d("Repository", "Pokemon fetched successfully: ${response.body()}")
+        } else {
+            Log.e("Repository", "Failed to fetch Pokemon: ${response.code()}")
+        }
+        return response
     }
 
     override suspend fun getPokemonFromDB(): List<Pokemon> {
